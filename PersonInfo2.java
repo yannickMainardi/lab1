521faea189ce72d7c37129ec05607c491b8227a5
Justin Leach <jleac092@uotttawa.ca>
2019-09-17 17:32:10 -0400

    Branch commit

--- PersonInfo2.java
+++ PersonInfo2.java
@@ -3,7 +3,7 @@ public class PersonInfo2 {
     public static String firstName = "Justin";
     public static String lastName = "Leach";
     public static int studentNo = 300091487;
-    public static String description = "Student qwerqwerqwer";
+    public static String description = "Student Test";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
